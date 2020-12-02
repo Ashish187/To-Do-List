ef9d8147e99fa1fb46d73475f501fea7c1f8aab5
Ashish187 <ashishanand16086@gmail.com>
2020-12-02 12:32:13 +0530

    second commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// // var und = undefined;
+// // console.log(und);
+// // var a = true;
+// // console.log(a)
+// var arr = [1,2,3,4,5,6];
+
+// // arr.forEach(function(element){
+// //     console.log(element);
+// // })
+// let mydate = new Date();
+// console.log(mydate);
+
+// let string = "Ashish Anand is a good boy";
+// console.log(string);
